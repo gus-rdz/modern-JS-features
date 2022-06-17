3cad5e4767f5505a1b6eb77ed1810c61396c4816
Gustavo Rodriguez <gustavo7rdz@gmail.com>
2022-06-16 22:24:36 -0500

    array test

--- script.js
+++ script.js
@@ -27,3 +27,12 @@ const restaurant = {
     },
   },
 };
+
+// Arrays
+// var is_array = function(input) {
+//   if (toString.call(input) === "[object Array]")
+//     return true;
+//   return false;
+//     };
+// console.log(is_array('w3resource'));
+// console.log(is_array([1, 2, 4, 0]));
